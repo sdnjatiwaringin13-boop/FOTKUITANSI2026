1eb299c772961687dc0c8c78d53724244cc0271d
sdnjatiwaringin13-boop <sdnjatiwaringin.13@gmail.com>
2026-09-21 16:09:14 +0700

    Set API_URL to the production endpoint

--- public/config.js
+++ public/config.js
@@ -9,7 +9,7 @@
 //
 // JANGAN gunakan URL /dev.
 // ==========================================================
-const API_URL = "";
+const API_URL = "https://script.google.com/macros/s/AKfycbz6ojgD9oPbMx9dJV_xvqp82UicAgVm7xVm6WuwtG7-qgme9QmOlEzST2Ngpwkr/exec";
 
 window.APP_CONFIG = {
   API_URL
