45bab4ee217bf0b728d1e727b75f6a1cd3ffe85c
sdnjatiwaringin13-boop <sdnjatiwaringin.13@gmail.com>
2026-09-21 17:38:30 +0700

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,3 +1,3 @@
 // Isi dengan URL Web App Google Apps Script yang berakhiran /exec.
-const API_URL = "";
+const API_URL = "https://script.google.com/macros/s/AKfycbw9zHpxoIHr7w1Zl_Fct3l7t6iW-gVvw_mi86JxJNp3FdDMG5UZXa2qvWldNztZoNw/exec";
 window.APP_CONFIG = { API_URL };
